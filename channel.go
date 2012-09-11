bd61b4b627b5959eed514d493146a09c14df0e00
Carl Johnson <carl@carlsensei.com>
2012-09-10 22:29:52 -1000

    Stubbed out.

--- channel.go
+++ channel.go
@@ -3,58 +3,73 @@ package goutils
 import "fmt"
 
 type Channel interface {
-    Each(func(Data))
-    Map(func(Data) Data) DataChannel
-    String() string
-    Any(func(Data) bool) bool
-    All(func(Data) bool) bool
+	Each(func(Data))
+	Map(func(Data) Data) Channel
+	String() string
+	Any(func(Data) bool) bool
+	All(func(Data) bool) bool
 }
 
-func NewChannel() dataChannel {}
-func NewChannelFromSlice() dataChannel {}
+func NewChannel() Channel {
+	return make(dataChannel)
+}
+func NewChannelFromSlice() Channel {
+	return make(dataChannel)
+}
 
 type dataChannel chan Data
 
 func (c dataChannel) Each(f func(Data)) {
-    for v := range c {
-        f(v)
-    }
+	for v := range c {
+		f(v)
+	}
 }
 
-func (c dataChannel) Map(f func(Data) Data) dataChannel {
-    r := make(DataChannel)
-    go func() {
-        for v := range c {
-            c <- f(v)
-        }
-        close(r)
-    }()
-    return r
+func (c dataChannel) Map(f func(Data) Data) Channel {
+	r := make(dataChannel)
+	go func() {
+		for v := range c {
+			c <- f(v)
+		}
+		close(r)
+	}()
+	return r
 }
 
 func (c dataChannel) String() string {
-    s = ""
-    for v := range c {
-        s += fmt.Sprint(v)
-    }
-    return s
+	s := ""
+	for v := range c {
+		s += fmt.Sprint(v)
+	}
+	return s
 }
 
-func (c dataChannel) Any() bool {
+func (c dataChannel) Any(func(Data) bool) bool {
+	return true
 }
 
-func (c dataChannel) All() bool {
+func (c dataChannel) All(func(Data) bool) bool {
+	return true
 }
 
 type numberChannel chan Number
 
-func NewNumberChannel() numberChannel {}
-func NewNumberChannelFromSlice() numberChannel {}
-
+func NewNumberChannel() numberChannel {
+	return make(numberChannel)
+}
+func NewNumberChannelFromSlice() numberChannel {
+	return make(numberChannel)
+}
 
 type stringChannel chan string
 
-func NewStringChannel() stringChannel {}
-func NewStringChannelFromSlice() stringChannel {}
+func NewStringChannel() stringChannel {
+	return make(stringChannel)
+}
+func NewStringChannelFromSlice() stringChannel {
+	return make(stringChannel)
+}
 
-func NewStringChannelFromFile(filename string) stringChannel {}
+func NewStringChannelFromFile(filename string) stringChannel {
+	return make(stringChannel)
+}
