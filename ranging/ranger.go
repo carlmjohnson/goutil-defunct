90fb9da078893b0ce5369af959b530ec99fec018
Carl Johnson <carl@carlsensei.com>
2012-10-24 16:04:10 -1000

    No need for a closure.

--- ranging/ranger.go
+++ ranging/ranger.go
@@ -5,31 +5,41 @@ import (
 	"github.com/earthboundkid/goutil/channel"
 )
 
+func positiveRange(from, to, step goutil.Number, ch channel.NumberChannel) {
+	for v := from; v < to; v += step {
+		ch <- v
+	}
+	close(ch)
+}
+
+func negativeRange(from, to, step goutil.Number, ch channel.NumberChannel) {
+	for v := from; to < v; v += step {
+		ch <- v
+	}
+	close(ch)
+}
+
 func FromToStep(from, to, step goutil.Number) channel.NumberChannel {
 
 	ch := channel.NewNumberChannel()
 
 	if step > 0 {
 		//If the Step is positive then wait for v to be greater than Stop
-		go func() {
-			for v := from; v < to; v += step {
-				ch <- v
-			}
-			close(ch)
-		}()
+		go positiveRange(from, to, step, ch)
 	} else {
 		//And vice versa, if negative, we need to wait for v to be lesser
-		go func() {
-			for v := from; to < v; v += step {
-				ch <- v
-			}
-			close(ch)
-		}()
+		go negativeRange(from, to, step, ch)
 	}
 
 	return ch
 }
 
 func To(to goutil.Number) channel.NumberChannel {
-	return FromToStep(0, to, 1)
+
+	ch := channel.NewNumberChannel()
+
+	go positiveRange(0, to, 1, ch)
+
+	return ch
+
 }
